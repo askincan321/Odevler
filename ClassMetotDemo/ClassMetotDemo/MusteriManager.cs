786c69580523e918a585cb862d8c6aa2c1c58bc7
askincan321 <askincan321@gmail.com>
2021-09-30 14:37:20 +0300

    MetotOdevi

---
+++ ClassMetotDemo/ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void Ekle (Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Eklendi :"+ musteri.Ad+""+ musteri.Soyad +""+ musteri.Sehir);
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + "Müşteri Silindi:" + musteri.Ad + "" + musteri.Soyad);
+        }
+
+        public void guncelle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id +" Müşteri Güncellendi:" + musteri.Ad +" " + musteri.Soyad);
+        }
+
+        
+    }
+}
---
+++ ClassMetotDemo/ClassMetotDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Ebru";
+            musteri1.Soyad = "Terzioğlu";
+            musteri1.Sehir = "İzmir";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Kuzey";
+            musteri2.Soyad = " Saygılı";
+            musteri2.Sehir = "Kocaeli";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Ad = "Aşkın Can";
+            musteri3.Soyad = "Gül";
+            musteri3.Sehir = "İstanbul";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+            MusteriManager musteriManager = new MusteriManager();
+            foreach (var musteri in musteriler)
+            {
+                musteriManager.Ekle(musteri);
+                musteriManager.guncelle(musteri);
+                musteriManager.Sil(musteri);
+
+                Console.Write("###");
+            }
+           
+
+
+
+        }
+    }
+}
